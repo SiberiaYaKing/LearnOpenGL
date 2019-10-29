267e9cea0e60d2b8234c92194329822d28e19d70
Sherlock <2041744819@qq.com>
2019-10-29 19:30:43 +0800

    upload opengl packages

--- LearnOpenGL/LearnOpenGL4_Texture/main.cpp
+++ LearnOpenGL/LearnOpenGL4_Texture/main.cpp
@@ -7,7 +7,12 @@
 #include <iostream>
 #include <math.h>
 
-#include "stb_image.h"
+#include <stb_image.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 using namespace std;
 
 
