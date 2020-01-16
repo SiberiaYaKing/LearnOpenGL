7c0e59ba7905cd6ab5b56e94680e6b490ce3aad2
sherlock lain <>
2020-01-16 15:07:39 +0800

    2020/1/16

--- LearnOpenGL_Practice/LearnOpenGL_Practice/Practice_main.cpp
+++ LearnOpenGL_Practice/LearnOpenGL_Practice/Practice_main.cpp
@@ -1,18 +1,14 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <stb_image.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 
 #include <imgui\imgui.h>
 #include <imgui/imgui_impl_glfw_gl3.h>
 
 #include <LearnOpenGL/shader.h>
 #include <LearnOpenGL/camera.h>
-#include <LearnOpenGL/model.h>
-#include <LearnOpenGL/assets_directory.h>
 #include <LearnOpenGL/opengl_window.h>
 #include <LearnOpenGL/texture_loader.h>
 
@@ -22,6 +18,7 @@ using namespace glm;
 using namespace std;
 
 void processInput(GLFWwindow *window);
+void DrawGUI();
 
 // settings
 const unsigned int SCR_WIDTH = 1280;
@@ -35,56 +32,58 @@ float lastY = (float)SCR_HEIGHT / 2.0;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-float vertices[] = {
-	// positions          // normals           // texture coords
-	 0.01f, -0.01f, -0.01f,  0.0f,  0.0f, -1.0f,  1.0f,  0.0f,
-	-0.01f, -0.01f, -0.01f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
-	 0.01f,  0.01f, -0.01f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
-	 0.01f,  0.01f, -0.01f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
-	-0.01f, -0.01f, -0.01f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
-	-0.01f,  0.01f, -0.01f,  0.0f,  0.0f, -1.0f,  0.0f,  1.0f,
+float cubeVertices[] = {
+	// positions          // texture Coords
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
 
-	-0.01f, -0.01f,  0.01f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
-	 0.01f, -0.01f,  0.01f,  0.0f,  0.0f,  1.0f,  1.0f,  0.0f,
-	 0.01f,  0.01f,  0.01f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
-	 0.01f,  0.01f,  0.01f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
-	-0.01f,  0.01f,  0.01f,  0.0f,  0.0f,  1.0f,  0.0f,  1.0f,
-	-0.01f, -0.01f,  0.01f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
 
-	-0.01f,  0.01f,  0.01f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
-	-0.01f,  0.01f, -0.01f, -1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
-	-0.01f, -0.01f, -0.01f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
-	-0.01f, -0.01f, -0.01f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
-	-0.01f, -0.01f,  0.01f, -1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
-	-0.01f,  0.01f,  0.01f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
 
-	 0.01f,  0.01f, -0.01f,  1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
-	 0.01f,  0.01f,  0.01f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
-	 0.01f, -0.01f, -0.01f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
-	 0.01f, -0.01f, -0.01f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
-	 0.01f,  0.01f,  0.01f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
-	 0.01f, -0.01f,  0.01f,  1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
 
-	-0.01f, -0.01f, -0.01f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
-	 0.01f, -0.01f, -0.01f,  0.0f, -1.0f,  0.0f,  1.0f,  1.0f,
-	 0.01f, -0.01f,  0.01f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
-	 0.01f, -0.01f,  0.01f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
-	-0.01f, -0.01f,  0.01f,  0.0f, -1.0f,  0.0f,  0.0f,  0.0f,
-	-0.01f, -0.01f, -0.01f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
-
-	 0.01f,  0.01f, -0.01f,  0.0f,  1.0f,  0.0f,  1.0f,  1.0f,
-	-0.01f,  0.01f, -0.01f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f,
-	 0.01f,  0.01f,  0.01f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
-	 0.01f,  0.01f,  0.01f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
-	-0.01f,  0.01f, -0.01f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f,
-	-0.01f,  0.01f,  0.01f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
-vec3 cubePos(0.0f);
+
+vec3 gizmoPos(0.5f);
 vec2 scrPos;
 
+
 int main() {
+	// window init -------------
 	OpenGLWindow window;
 	try {
 		window.initWindow(SCR_WIDTH, SCR_HEIGHT, "Practice");
@@ -105,28 +104,40 @@ int main() {
 		lastX = xpos, lastY = ypos;
 		camera.ProcessMouseMovement(xoffset, yoffset);
 	});
-	//window.setScrollCallback([](GLFWwindow* window, double xoffset, double yoffset) {
-	//	camera.ProcessMouseScroll(yoffset);
-	//});
+	window.setScrollCallback([](GLFWwindow* window, double xoffset, double yoffset) {
+		camera.ProcessMouseScroll(yoffset);
+	});
+	//---------------------
 
 	glEnable(GL_DEPTH_TEST);
 
-	Shader shader(dir_shaders+"lampShader.vs",dir_shaders+"lampShader.fs");
+	//cubeShader and texture init ----------------
+	Shader cubeShader("cubeShader.vs","cubeShader.fs");
+	TextureLoader cubeTex("container.jpg");
+	TextureLoader gizmoTex("gizmo.png");
+	cubeShader.use();
+	cubeShader.setInt("texture1", 0);
+	//----------------------
 
+	//vertex data init -----------------------
 	unsigned cubeVBO, cubeVAO;
 	glGenBuffers(1, &cubeVBO);
 	glGenVertexArrays(1, &cubeVAO);
 	glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
 	glBindVertexArray(cubeVAO);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3*sizeof(float)));
 	glEnableVertexAttribArray(0);
+	//---------------------------
 
-	////++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-	//ImGui::CreateContext();
-	//ImGui_ImplGlfwGL3_Init(window.getWindowPtr(), true);
-	//ImGui::StyleColorsDark();
-	////++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+	//init GUI--------------
+	ImGui::CreateContext();
+	ImGui_ImplGlfwGL3_Init(&window, true);
+	ImGui::StyleColorsDark();
+	//-----------------------------
 
 	while (!window.isWindowClosed()) {
 		float currentFrame = glfwGetTime();
@@ -138,25 +149,51 @@ int main() {
 
 		processInput(&window);
 
+		//Draw big cube  ---------------------
 		mat4 projection = perspective(radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		mat4 view = camera.GetViewMatrix();
 		mat4 model = mat4(1.0f);
-		shader.use();
-		shader.setMat4("projection", projection);
-		shader.setMat4("view", view);
-		shader.setMat4("model", model);
-
-		vec4 clipPos = projection * view*model*vec4(cubePos, 1.0f);
-		scrPos = vec2((clipPos.x / clipPos.w+1)*0.5f*SCR_WIDTH, 
-			(clipPos.y / clipPos.w-1)*-0.5f*SCR_HEIGHT);
-
-		cout << "x: " << scrPos.x << " y: " << scrPos.y<< endl;
-
+		cubeShader.use();
+		cubeShader.setMat4("projection", projection);
+		cubeShader.setMat4("view", view);
+		cubeShader.setMat4("model", model);
 		glBindVertexArray(cubeVAO);
+		TextureLoader::activeTexture(GL_TEXTURE0);
+		cubeTex.bindTexture();
 		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glBindVertexArray(0);
+		//------------------------------
+
+		//Draw little red cube(gizmo)---------------------
+		model = translate(model, gizmoPos);
+		model = scale(model, vec3(0.02f));
+		cubeShader.setMat4("model", model);
+		glBindVertexArray(cubeVAO);
+		TextureLoader::activeTexture(GL_TEXTURE0);
+		gizmoTex.bindTexture();
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glBindVertexArray(0);
+		//--------------------------------------
+
+		//将小红块的坐标从世界空间转换到裁剪空间
+		vec4 clipPos = projection * view*model*vec4(gizmoPos, 1.0f);
+		//齐次除法&屏幕坐标映射
+		float x = (clipPos.x / clipPos.w + 1)*0.5f*SCR_WIDTH;
+		float y = (clipPos.y / clipPos.w - 1)*-0.5f*SCR_HEIGHT;
+		scrPos = vec2(x, y);
+		////输出屏幕坐标
+		//if (x >= 0 && x <= SCR_WIDTH && y >= 0 && y <= SCR_HEIGHT)
+		//	cout << "x: " << scrPos.x << " y: " << scrPos.y << endl;
+		//else cout << "gizmo out of screen range" << endl;
 		
+		DrawGUI();
+
 		window.swapBuffersAndPollEvents();
 	}
+
+	//Close GUI
+	ImGui_ImplGlfwGL3_Shutdown();
+	ImGui::DestroyContext();
 
 	glDeleteVertexArrays(1,&cubeVAO);
 	glDeleteBuffers(1,&cubeVBO);
@@ -176,4 +213,20 @@ void processInput(GLFWwindow * window)
 		camera.ProcessKeyboard(LEFT, deltaTime);
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+inline void DrawGUI() {
+	ImGui_ImplGlfwGL3_NewFrame();
+	{
+		ImGui::Begin("Screen Postion");
+		float arr[2] = { scrPos.x,scrPos.y };
+		if (arr[0] >= 0 && arr[0] <= SCR_WIDTH && arr[1] >= 0 && arr[1] <= SCR_HEIGHT)
+			ImGui::InputFloat2("Target ScrPos", arr);
+		else ImGui::Text("Target out of screen");
+		ImGui::Text("(Target is the little red cube(gizmo)");
+		ImGui::Text(" of the big cube's corner)");
+		ImGui::End();
+	}
+	ImGui::Render();
+	ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 }
